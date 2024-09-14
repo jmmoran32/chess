719718acfb5c1242c5b442b95418407a3bb55db0
Jonathan Moran <jmmoran@byu.edu>
2024-09-14 05:39:25 -0600

    Filled out most functions for ChessBoard. Linter shows no errors, but haven't tried building yet.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,9 +7,18 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private ChessPiece[][] board;
+    private ChessPiece[] whiteBox;
+    private byte wbn;
+    private ChessPiece[] blackBox;
+    private byte bbn;
 
     public ChessBoard() {
-        
+        this.board = new ChessPiece[8][8];
+        this.whiteBox = new ChessPiece[32];
+        this.blackBox = new ChessPiece[32];
+        wbn = 0;
+        bbn = 0;
     }
 
     /**
@@ -19,7 +28,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        this.board[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +39,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return this.board[position.getRow()][position.getColumn()];
     }
 
     /**
@@ -38,6 +47,59 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        this.board[0][0] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        this.board[0][1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        this.board[0][2] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        this.board[0][3] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+        this.board[0][4] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+        this.board[0][5] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        this.board[0][6] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        this.board[0][7] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        this.board[7][0] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        this.board[7][1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        this.board[7][2] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        this.board[7][3] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+        this.board[7][4] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        this.board[7][5] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        this.board[7][6] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        this.board[7][7] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        for(byte i = 0; i < 8; i++) this.board[1][i] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+        for(byte i = 0; i < 8; i++) this.board[6][i] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    }
+
+    /**
+     * places a piece in a box
+     */
+    public void boxPiece(ChessPosition position) {
+        int m = position.getRow();
+        int n = position.getColumn();
+        if(this.board[m][n] == null) {
+            System.out.println(String.format("This position (%d:%d) is already empty!", position.getRow(), position.getColumn()));
+            return; 
+        }
+
+        ChessPiece p = this.board[m][n];
+        if(p.getTeamColor() == ChessGame.TeamColor.WHITE) {
+           this.whiteBox[wbn++] = p; 
+        }
+        else if(p.getTeamColor() == ChessGame.TeamColor.BLACK) {
+           this.blackBox[bbn++] = p; 
+        }
+        this.board[m][n] = null;
+
+        return;
+    }
+
+    /**
+     * clears the entire board, and the boxes
+     */
+    private void clearBoard() {
+        for(byte i = 0; i < 8; i++) {
+            for(byte j = 0; j < 8; j++) {
+                this.board[i][j] = null;
+            }
+        }
+        for(byte i = 0; i < 64; i++) this.whiteBox[i] = null;
+        for(byte i = 0; i < 64; i++) this.blackBox[i] = null;
     }
 }
