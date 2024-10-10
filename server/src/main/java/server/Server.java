225885827836c49a2c81e3b6e4902c9382866435
Jonathan Moran <jmmoran@byu.edu>
2024-10-10 15:29:04 -0600

    prototyped api functions in server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,6 +10,14 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::registration);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
+        Spark.delete("/db", this::clearApplication);
+        Spark.exception(ResponseException.class, this::exceptionHandler);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
@@ -17,6 +25,7 @@ public class Server {
         Spark.awaitInitialization();
         return Spark.port();
     }
+
 
     public void stop() {
         Spark.stop();
