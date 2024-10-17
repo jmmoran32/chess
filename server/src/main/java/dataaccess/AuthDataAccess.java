8037f37ee30c6826411e70b2fbbd5b58d6044c93
Jonathan Moran <jmmoran@byu.edu>
2024-10-17 16:22:38 -0600

    Added some forgotten functions to AuthDataAccess. Might be done with the DAOs for now.

--- server/src/main/java/dataaccess/AuthDataAccess.java
+++ server/src/main/java/dataaccess/AuthDataAccess.java
@@ -6,11 +6,18 @@ import java.util.ArrayList;
 public class AuthDataAccess {
     private static final ArrayList<dbobjects.AuthData> table = new ArrayList<dbobjects.AuthData>();
 
-    public static void CreateAuth(String authToken, String username) throws DataAccessException {
+    public static void createAuth(String authToken, String username) throws DataAccessException {
         for(dbobjects.AuthData record : table)
-            if(record.authToken() == authToken)
+            if(record.authToken().equals(authToken))
                 throw new DataAccessException(String.format("Authtoken %d already exists in AuthData as record no %ld.", record.authToken(), record.id()));
         AuthDataAccess.table.add(new dbobjects.AuthData(authToken, username));
+    }
+
+    public static String getAuthToken(String authToken) {
+        for(dbobjects.AuthData record : table)
+            if(record.authToken().equals(authToken))
+                return record.authToken();
+        return null;
     }
 
     public static void write() {
