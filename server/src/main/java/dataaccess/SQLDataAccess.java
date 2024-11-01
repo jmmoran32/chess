b4d5de824741a1ee0788201c56fcad0d6a499d1a
Jonathan Moran <jmmoran@byu.edu>
2024-10-31 19:50:10 -0600

    Added an abstract class to be inherited by each of the data access interfaces. The purpose of this abstract class is to init the database on startup, and create tables if they do not already exist.

---
+++ server/src/main/java/dataaccess/SQLDataAccess.java
@@ -0,0 +1,41 @@
+package dataaccess;
+
+import java.sql.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public abstract class SQLDataAccess {
+    private static final Connection connection;
+
+    static  {
+        try {
+            DBManager.createDatabase();
+        }
+        catch(DataAccessException e) {
+            throw new RuntimeException(String.format("Failed to create the database: %s", e.getMessage()));
+        }
+        try {
+            connection = DBManager.getConnection();
+        }
+        catch(DataAccessException e) {
+            throw new RuntimeException(String.format("Failed to create a connection: %s", e.getMessage()));
+        }
+        File init = new File("server/src/main/resources/init.sql");
+        StringBuilder initStatement = new StringBuilder();
+        try(Scanner s = new Scanner(init)) {
+            while(s.hasNextLine()) {
+                initStatement.append(s.nextLine());
+            }
+        }
+        catch(FileNotFoundException e) {
+            throw new RuntimeException(String.format("Init file could not be found: %s", e.getMessage()));
+        }
+        try(PreparedStatement statement = connection.prepareStatement(initStatement.toString())) {
+            statement.executeUpdate();
+        }
+        catch(SQLException e) {
+            throw new RuntimeException(String.format("There was a problem executing init: %s", e.getMessage()));
+        }
+    }
+}
