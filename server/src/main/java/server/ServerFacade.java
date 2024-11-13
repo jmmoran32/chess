de227ff6b23c59ebfe6a9086ee79ac9bc0c93e9b
Jonathan Moran <jmmoran@byu.edu>
2024-11-13 12:10:07 -0700

    prototyped the first endpoint interface. Have not tested it yet.

--- server/src/main/java/server/ServerFacade.java
+++ server/src/main/java/server/ServerFacade.java
@@ -6,12 +6,39 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.*;
 import exception.ResponseException;
+import dbobjects.GameData;
+import java.util.ArrayList;
+import chess.ChessGame;
 
 public class ServerFacade {
     private String url;
 
     public ServerFacade(String url) {
         this.url = url;
+    }
+
+    public String registration(String username, String password, String email) {
+        RegisterRequest req = new RegisterRequest(username, password, email);
+        RegisterResponse res = this.makeRequest("POST", "/user", req, RegisterResponse.class); 
+        return res.authToken();
+    }
+
+    public String login(String username, String password) {
+    }
+
+    public void logout(String authToken) {
+    }
+
+    public ArrayList<GameData> listGames(String authToken) {
+    }
+
+    public int createGame(String authToken, String gameName) {
+    }
+
+    public void joinGame(String authToken, ChessGame.TeamColor color, int gameID) {
+    }
+
+    public void clearApplication() {
     }
 
     private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws Exception {
