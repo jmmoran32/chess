b8c342d02dc614dd9ca2c8c6549c1ea829c91345
dummyName <dummy@email.com>
2024-11-16 05:02:24 -0700

    added a format function for printing the board all colorful like

--- client/src/main/java/ui/UI.java
+++ client/src/main/java/ui/UI.java
@@ -6,6 +6,7 @@ import facade.ResponseException;
 import java.util.HashMap;
 import chess.ChessGame;
 import java.util.ArrayList;
+import facade.ChessGameRecord;
 
 public class UI {
     private static String header;
@@ -15,9 +16,22 @@ public class UI {
     private static Scanner s;
     private static boolean isLoggedIn = false;
     private static boolean quit = false;
-    private static final HashMap<Integer, ChessGame> games = new HashMap<Integer, ChessGame>();
+    private static final HashMap<String, ChessGame> gamesMap = new HashMap<String, ChessGame>();
+
+    private static final String WHITE_TILE = "43m";
+    private static final String BLACK_TILE = "45m";
+    private static final String WHITE_PIECE = "32;";
+    private static final String BLACK_PIECE = "31;";
+    private static String WHITE_ON_WHITE;
+    private static String WHITE_ON_BLACK;
+    private static String BLACK_ON_WHITE;
+    private static String BLACK_ON_BLACK;
 
     public static void run(String url) {
+        //WHITE_ON_BLACK = "\033[" + WHITE_PIECE + BLACK_TILE + "m%c";
+        //BLACK_ON_BLACK = "\033[" + BLACK_PIECE + BLACK_TILE + "m%c";
+        //WHITE_ON_WHITE = "\033[" + WHITE_PIECE + WHITE_TILE + "m%c";
+        //BLACK_ON_WHITE = "\033[" + BLACK_PIECE + WHITE_TILE + "m%c";
         header = "[Logged out]$ "; 
         facade = new ServerFacade(url);
         s = new Scanner(System.in);
@@ -38,8 +52,7 @@ public class UI {
                     return;
                 }
             }
-        }
-        return;
+        } return;
     }
 
     private static boolean handleException(Exception e) {
@@ -185,13 +198,7 @@ public class UI {
                 return;
 
             case "list":
-                try {
-                ArrayList<ChessGame> gameList = facade.listGames(authToken);
-                System.out.println(gameList.get(0).getBoard().toString());
-                }
-                catch(Exception e) {
-                    System.out.println("Unsuccessful list: " + e.getMessage());
-                }
+                updateGameList();
                 return;
             case "join":
                 return;
@@ -206,9 +213,73 @@ public class UI {
         }
     }
 
-    /*
     private static void updateGameList() throws Exception {
-        ArrayList<ChessGame> gameList = facade.listGames(authToken);
+        ArrayList<ChessGameRecord> gameList = facade.listGames(authToken);
+        ArrayList<String> keysFromDB = new ArrayList<String>();
+
+        for(ChessGameRecord r : gameList) {
+            gamesMap.put(r.gameID(), r.game());
+            keysFromDB.add(r.gameID());
+        }
+
+        for(String i : gamesMap.keySet()) {
+            if(!keysFromDB.contains(i)) {
+                gamesMap.remove(i);
+            }
+        }
     }
-    */
+
+    private static void printGameList() {
+    }
+
+    private String[] formattedBoard(ChessGame game) {
+        char[] whiteBoard = new char[64];
+        char[] blackBoard;
+        char[] bucketBoard;
+        char bucketChar;
+        String bucketTile;
+        String bucketPiece;
+        
+        whiteBoard = game.getBoard().serialize().toCharArray();
+        if(game.getTeamTurn() == ChessGame.TeamColor.BLACK) {
+            blackBoard = new char[64];
+            int sum;
+            for(int i = 0; i < 64; i++) {
+                blackBoard[i] = whiteBoard[63 - i];
+            }
+            bucketBoard = blackBoard;
+        }
+        else {
+            bucketBoard = whiteBoard;
+        }
+        StringBuilder sb = new StringBuilder();
+        String board[] = new String[8];
+        boolean whitetile = true;
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                bucketChar = bucketBoard[i * 8 + j];
+                if(bucketChar < 91) {
+                    bucketPiece = WHITE_PIECE;
+                }
+                else {
+                    bucketPiece = BLACK_PIECE;
+                }
+                if(whitetile) {
+                    bucketTile = WHITE_TILE;
+                }
+                else {
+                    bucketTile = BLACK_TILE;
+                }
+                sb.append("\033[");
+                sb.append(bucketPiece);
+                sb.append(bucketTile);
+                sb.append(bucketChar);
+            }
+            sb.append("\033[0m\n");
+            board[i] = sb.toString();
+            sb = new StringBuilder();
+            whitetile ^= true;
+        }
+        return board;
+    }
 }
