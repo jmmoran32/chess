5a013b502e3f1a3c410959fa6a0a8a6700a12aa0
Jonathan Moran <jmmoran@byu.edu>
2024-11-25 22:53:09 -0700

    Added a serialize and deserialize function to the UserGameCommand class

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -29,6 +29,58 @@ public class UserGameCommand {
         RESIGN
     }
 
+    public String serialize() {
+        StringBuilder sb = new StringBuilder();
+        switch(this.commandType) {
+            case CONNECT:
+                sb.append("CONNECT\t");
+                break;
+            case MAKE_MOVE:
+                sb.append("MAKE_MOVE\t");
+                break;
+            case LEAVE:
+                sb.append("LEAVE\t");
+                break;
+            case RESIGN:
+                sb.append("RESIGN\t");
+                break;
+            default:
+                return null;
+        }
+
+        sb.append(this.authToken);
+        sb.append('\t');
+        sb.append(Integer.toString(this.gameID));
+        return sb.toString();
+    }
+
+    public static UserGameCommand deSerialize(String serial) {
+        String serialArray[] = serial.split("\t");
+        if(serialArray.length < 3) {
+            return null;
+        }
+
+        CommandType ctype;
+        switch(serialArray[0]) {
+            case "CONNECT":
+                ctype = CommandType.CONNECT;
+                break;
+            case "MAKE_MOVE":
+                ctype = CommandType.MAKE_MOVE;
+                break;
+            case "LEAVE":
+                ctype = CommandType.LEAVE;
+                break;
+            case "RESIGN":
+                ctype = CommandType.RESIGN;
+                break;
+            default:
+                return null;
+        }
+        
+        return new UserGameCommand(ctype, serialArray[1], Integer.parseInt(serialArray[2]));
+    }
+
     public CommandType getCommandType() {
         return commandType;
     }
